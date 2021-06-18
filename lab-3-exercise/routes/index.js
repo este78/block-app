fc75eb80b1aa01f4874a7647fc17c62ffbc3fc7d
este78 <araujofe@tcd.ie>
2021-06-18 21:24:26 +0100

    creating new pages lab 3

--- lab-3-exercise/routes/index.js
+++ lab-3-exercise/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Page 1' });
+});
+
+/* GET home page. */
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Page 2' });
+});
+
 module.exports = router;
