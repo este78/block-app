ceeee28a54a81bdf9f62f10e05c8cc754b3f1be5
este78 <araujofe@tcd.ie>
2021-07-02 19:16:16 +0100

    Lab 5 Initial commit. Provided by Lecturer Josh

--- lab-3-balance_exercise/public/javascripts/transferEth.js
+++ lab-3-balance_exercise/public/javascripts/transferEth.js
@@ -4,19 +4,19 @@ var Tx = require("ethereumjs-tx").Transaction
 var Web3 = require("web3")
 var web3_instance = new Web3("http://127.0.0.1:7545");
 
-async function sendEth() {
+async function sendEth(here) {
     try {
-        var amount = '3'
-        var gasFees = '2'
+        var amount = '5'
+        var gasFees = '1'
         var gasLimit = '300000'
-        
+        var toAddress = here.String()
     
         var privateKey = Buffer.from('b2f7098f6678b19a811704f31f549b916913df1fb403cd497bb8ccacc1639edd', "hex")
         var rawTransaction = {
             nonce: await web3_instance.eth.getTransactionCount("0x7134a1cCa554180b9fbdDaB7816FB034A6e93FF6"),
             gasPrice: web3_instance.utils.numberToHex(gasFees),
             gasLimit: web3_instance.utils.numberToHex(gasLimit),
-            to: '0x3da0A28771C77198E88dC3739A0bD44f84d4129F',
+            to: toAddress,
             value: web3_instance.utils.numberToHex(web3_instance.utils.toWei(amount, 'ether'))
         }
     
