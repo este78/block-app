a58838981892a428c50c28154552271ba93f30a4
este78 <araujofe@tcd.ie>
2021-08-07 19:13:27 +0100

    migration error hook

--- primeLottery/contracts/Lottery.sol
+++ primeLottery/contracts/Lottery.sol
@@ -1,13 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract Lottery {
+    function factorial(uint x) public pure returns (uint) {
+        uint ans = x;
+        // for loop
+        for (uint i = x-1; i > 1; i--) {
+            ans = x * ans;
+        }
+
+        return ans;
+    }
   function isPrime(uint256 _numberDrawed) pure public returns(bool){
-        uint256 numberDrawed = _numberDrawed;
-        for(uint i = 2; i < numberDrawed; i++){
-            if(numberDrawed % i == 0 ){
+        uint256 num = _numberDrawed;
+        for(uint i = 2; i < num; i++){
+            if(num % i == 0 ){
                 return false;
             }
-            return numberDrawed > 1;
+            return num > 1;
         }
     }
 }
--- primeLottery/test/TestLottery.sol
+++ primeLottery/test/TestLottery.sol
@@ -7,7 +7,10 @@ import "../contracts/Lottery.sol";
 contract TestLottery {
     // The address of the lottery contract to be tested
     Lottery lottery = Lottery(DeployedAddresses.Lottery());
-
+    
+    function testLoop() public {
+        Assert.equal(lottery.factorial(5), 625, "Loop result");
+    }
     function testIsPrime() public {
       Assert.equal(lottery.isPrime(13), true, "Lottery result");   
     }
