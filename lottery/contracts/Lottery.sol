785876c6007c9e7bc2f4e47dd0760397b2739ead
este78 <araujofe@tcd.ie>
2021-08-08 21:42:32 +0100

    lottery draws numbers 1-100 instead of 0-99

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -65,20 +65,24 @@ contract Lottery{
 
    //Player draw a number  
    function tryYourLuck() payable public {
+       //conditions
        require(msg.value == 1 ether, "You need to pay 1 ether to play");
        require(address(this).balance > 3 ether, "Machine has no funds");
+       //2 function calls for the draw (draw number, chack if winner)
        uint myNumber = rand();
        bool winner = isPrime(myNumber);
+       // transfer ether if winner
        if (winner){
            msg.sender.transfer(3000000000000000000);
        }
+       //Log the draw in a mapping 
        draws[drawIndex] = Draw(drawIndex, myNumber, winner, msg.sender);
        drawIndex++;
        emit DrawCreated(myNumber, winner, msg.sender);
    }
   
   
-   //pseudo-random number generator
+   //pseudo-random number generator. Source: https://stackoverflow.com/questions/58188832/solidity-generate-unpredictable-random-number-that-does-not-depend-on-input
    function rand() public view  returns(uint256) {
         uint256 seed = uint256(keccak256(abi.encodePacked(
             block.timestamp + block.difficulty +
@@ -87,11 +91,11 @@ contract Lottery{
             ((uint256(keccak256(abi.encodePacked(msg.sender)))) / (block.timestamp)) +
             block.number
         )));
-    
-        return (seed - ((seed / 100) * 100));
+        //Draw numbers 1-100 (there are 25 prime numbers, chance of winning 1/4)
+        return ((seed - ((seed / 100) * 100))+1);
     }
 
-   //only prime numbers get a prize
+   //only prime numbers get a prize. Adapted from: https://stackoverflow.com/questions/40200089/number-prime-test-in-javascript
     function isPrime(uint256 _numberDrawed) pure public returns(bool){
         uint256 num = _numberDrawed;
         uint256 index = 2;
